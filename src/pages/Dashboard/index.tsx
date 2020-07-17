1ca9195373e9799327829a8f898fa5b2a83e1793
DevBacega <dev.victor.bacega@gmail.com>
2020-07-16 23:23:57 -0300

    Projeto de pesquisa no github feito no GoStack 12

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars0.githubusercontent.com/u/54642509?s=460&u=f3d271b3747eac6035027ed70229ab26d04089f0&v=4"
+        </a>
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
